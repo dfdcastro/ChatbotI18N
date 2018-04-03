a507f631cf445945085d28665abc90b22663b0a8
Diego Castro <dfdcastro@gmail.com>
2018-04-03 13:39:19 -0300

    Add project

---
+++ SampleI18N/Dialogs/RootDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using SampleI18N.Resource;
+
+namespace SampleI18N.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {           
+            await context.PostAsync(Strings.Hello);
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
